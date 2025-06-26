50ba2c2bde9993dbd83eb93959b87ba0fb26fae9
surajPatwa12 <surajpatwa8090@gmail.com>
2025-06-26 16:59:38 +0530

    problem8.py

--- python proogram/problem7.py
+++ python proogram/problem7.py
@@ -1,1 +1,7 @@
-ahygfuuh
+
+a=5
+b=16
+sum=a+b 
+print("The sum of a and b is: ", sum)
+# Output: The sum of a and b is: 21
+# The code has a syntax error. The print statement is not properly formatted.
---
+++ python proogram/problem8.py
@@ -0,0 +1,5 @@
+a=int(input("enter the no"))
+b=int(input("enter the no"))
+mul=a*b
+print("The multiplication of a and b is: ", mul)
+    
